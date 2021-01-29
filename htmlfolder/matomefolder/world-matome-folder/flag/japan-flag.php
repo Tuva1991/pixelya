02bfa73e98f778dba6656ff0dcbbaaab0c3392ab
Tuva1991 <70495097+Tuva1991@users.noreply.github.com>
2021-01-29 22:33:03 +0900

    Update japan-flag.php

--- htmlfolder/matomefolder/world-matome-folder/flag/japan-flag.php
+++ htmlfolder/matomefolder/world-matome-folder/flag/japan-flag.php
@@ -225,16 +225,13 @@
     $calc = fgets($fp);
     $calc = (int) $calc;
     $calc = $calc + 1;
-    //$fp1 = fopen("https://lit-fortress-24137.herokuapp.com/counter-japan-flag.txt","w+");
-    //fwrite($fp1,$calc);
-    //fclose($fp);
-    //fclose($fp1);
+    $fp1 = fopen("https://lit-fortress-24137.herokuapp.com/counter-japan-flag.txt","w+");
+    fwrite($fp1,$calc);
+    fclose($fp);
+    fclose($fp1);
     ?>
     <script type="text/javascript">
         var test = <?php echo json_encode($calc); ?>
-        console.log(test)
-        console.log("testさん")
-        result.innerHTML = test
     </script>
 </body>
 </html>
