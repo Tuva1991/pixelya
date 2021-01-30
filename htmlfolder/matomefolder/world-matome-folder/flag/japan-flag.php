398355e438c26313284dfaa946d0ca20490a5a0e
Tuva1991 <70495097+Tuva1991@users.noreply.github.com>
2021-01-30 15:54:09 +0900

    Update japan-flag.php

--- htmlfolder/matomefolder/world-matome-folder/flag/japan-flag.php
+++ htmlfolder/matomefolder/world-matome-folder/flag/japan-flag.php
@@ -222,17 +222,18 @@
     <script src="https://lit-fortress-24137.herokuapp.com/javascriptfolder/search.js"></script>
     <?php
     $link = "https://lit-fortress-24137.herokuapp.com/counter-japan-flag.txt";
-    $fp = fopen($link,"a+");
-    $calc = fgets($fp);
-    $clac = "1"
+    //$fp = fopen($link,"a+");
+    //$calc = fgets($fp);
+    $clac = "1";
     $calc = (int) $calc;
     $calc = $calc + 1;
     $calc = (string) $calc;
-    $hoge2 = fwrite($fp, $calc);
-    $hoge = fclose($fp);
+    //$hoge2 = fwrite($fp, $calc);
+    //$hoge = fclose($fp);
     ?>
     <script type="text/javascript">
         var clac = <?php echo json_encode($calc); ?>
+            link = <?php echo json_encode($link); ?>
             open = <?php echo json_encode($fp); ?>
             close = <?php echo json_encode($hoge); ?>
             hoge1 = <?php echo json_encode($hoge1); ?>
