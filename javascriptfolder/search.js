a953e3a5d236a674438da6be00587bf7c175d0ba
Tuva1991 <70495097+Tuva1991@users.noreply.github.com>
2021-01-30 23:44:15 +0900

    Update search.js

--- javascriptfolder/search.js
+++ javascriptfolder/search.js
@@ -1,14 +1,16 @@
 var date = ["世界","国旗","世界の建物"]
 var link = ["world-matome.html","world-matome-folder/flag.html" , "world-matome-folder/world-building.html"]
 var search_text = document.getElementById("search_text")
+var datecount = date.length
 // date は カテゴリー   link はカテゴリーと同期した リンク  search_textは htmlで入力したテキストボックス
 // x　は 回数のもの date と link を結びつけるのは この子の役目
 // a は forEach で使われる date の 順番にしてるやつ   b は true or false で 検索されたかされてないかを確かめる
+//datecount は for 文で使うもの
 function function_search()
 {
     let x = 0
     let b = false
-    for(let i = 0; i<30; i++)
+    for(let i = 0; i<datecount; i++)
     {
     console.log("search1")
     date.forEach(a =>
