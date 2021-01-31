e53db11440c7eece4fa726f2327ad6037ac5537f
Tuva1991 <70495097+Tuva1991@users.noreply.github.com>
2021-01-31 20:13:17 +0900

    Update index.php

--- index.php
+++ index.php
@@ -135,18 +135,31 @@
                 </li>
             </ul>
             <div　class="comment"><!-- コメント機能 -->
-                <text class="comment-title">コメント機能</text>
+                <text class="comment-title">コメント機能<br></text>
                 <text class="comment-subtitle">不適切なコメントは控えてください。<br>制限最大50文字</text>
                 <p>
-                    <input type="text" class="comment-text" maxlength="50">
-                    <input type="button" class="comment-button">
+                    <form action="index.php" method="post">
+                        <input type="text" class="comment-text" maxlength="50">
+                        <input type="button" class="comment-button" value= "送信">
+                    </form>    
                 </p>
             </div>
         </div>
         </div>
         <!--ここにコメント作りたい-->
     <?php
-
+    if(isset($_POST['comment']))
+    {
+        $comment = $_POST['comment'];
+        htmlspecialchars($comment, ENT_QUOTES, 'UTF-8');
+        $link = 'comment.txt';
+        $fp = fopen("comment.txt", "r");
+        $string = fgets($fp);
+        $string = $string.$comment;
+        fclose($fp);
+        $fp = null;
+        file_put_contents($link, $string);
+    }
     ?>
     <script src="javascriptfolder/search.js"></script>
 </body>
