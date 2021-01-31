b5999a438af312f0b4ee2604e80e3506ea56973d
Tuva1991 <70495097+Tuva1991@users.noreply.github.com>
2021-01-31 21:15:06 +0900

    Update index.php

--- index.php
+++ index.php
@@ -140,7 +140,7 @@
                 <p>
                     <form action = "index.php" method = "post">
                         <input type="text" class="comment-text" maxlength="50" name ="comment/">
-                        <input type="button" class="comment-button" value= "送信" onclick="comment();" id="comment-button">
+                        <input type="button" class="comment-button" value= "送信">
                     </form>
                 </p>
             </div>
