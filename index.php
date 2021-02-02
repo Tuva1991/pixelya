c661be88b0ea407147bd0a5420ba3c902ea28fe2
Tuva1991 <70495097+Tuva1991@users.noreply.github.com>
2021-02-02 14:33:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     $fp = fopen("comment.txt", "r");
     $string = fgets($fp);
-    $error = test;
+    $error = "test";
     if(isset($_POST['comment']))
     {
         $comment = $_POST['comment'];
